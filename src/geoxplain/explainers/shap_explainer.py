a64edc1195c95423ec1b0b73fe3c6a985a39d6c7
Khadem <khademgenu22@gmail.com>
2026-09-21 11:31:46 +0600

    init

--- src/geoxplain/explainers/shap_explainer.py
+++ src/geoxplain/explainers/shap_explainer.py
@@ -114,7 +114,7 @@ class ShapValues:
 
 
 def _to_float(base: Any) -> float:
-    return float(np.ravel(np.asarray(base, dtype=float))[0])
+    return float(np.ravel(np.asarray(base.replace('[', '').replace(']', ''), dtype=float))[0])
 
 
 class GeoShapExplainer:
